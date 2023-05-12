9a8d1596fbfb722041c2716a29bd0e171ed0014f
mattemugno <m.mugnai5@studenti.unipi.it>
2023-05-12 11:14:38 +0200

    prima rete testata con gtrsb

--- main.py
+++ main.py
@@ -1,36 +1,71 @@
 import os
 
+import numpy as np
 import tensorflow as tf
 from keras.preprocessing.image_dataset import image_dataset_from_directory
-from tensorflow import posit160
-from tensorflow.keras import backend as K
+from PIL import Image
+from sklearn.model_selection import train_test_split
+from tensorflow.python.keras import backend as K
 
 from utility import evaluate
 
-models_path = 'models'
+model_path = 'models'
 test_path = 'test_set'
+training_set = 'training_set'
 
 
 def load_model():
     model_name = 'GTSRB.h5'
-    K.set_floatx('posit160')
-    gtsrb_model = tf.keras.models.load_model(os.path.join(models_path, model_name))
+    #K.set_floatx('posit160')
+    model = tf.keras.models.load_model(os.path.join(model_path, model_name), compile=False)
     model.summary()
-    return gtsrb_model
+    model.compile(loss='categorical_crossentropy',
+                  optimizer='adam',
+                  metrics=['accuracy'])
+    return model
 
 
-def load_test_set(test_path, IMAGE_HEIGHT, IMAGE_WIDTH, BATCH_SIZE):
+'''def load_test_set(test_path, IMAGE_HEIGHT, IMAGE_WIDTH, BATCH_SIZE):
     test_set = image_dataset_from_directory(
         test_path,
         label_mode='categorical',
         shuffle=False,
+        labels='inferred',
         image_size=(IMAGE_HEIGHT, IMAGE_WIDTH),
         batch_size=BATCH_SIZE)
-    return test_set
+    test_set.cardinality().numpy()
+    return test_set'''
 
 
-# Press the green button in the gutter to run the script.
+def load_test_set():
+    images = training_set
+    data = []
+    labels = []
+    classes = 43
+    for i in range(classes):
+        if i < 10:
+            img_path = os.path.join(images, "0000" + str(i))
+        else:
+            img_path = os.path.join(images, "000" + str(i))
+        for img in os.listdir(img_path):
+            if ".csv" in img:
+                continue
+            im = Image.open(img_path + '/' + img)
+            im = im.resize((32, 32))
+            im = np.array(im)
+            data.append(im)
+            labels.append(i)
+
+    data = np.array(data)
+    labels = np.array(labels)
+
+    x = data.astype('float32')
+    y = tf.keras.utils.to_categorical(np.array(labels))
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1, shuffle=True, stratify=y)
+    return x_test, y_test
+
+
 if __name__ == '__main__':
-    model = load_model()
-    test_set = load_test_set()
-    # evaluate(model, test_set)
+    gtsrb_model = load_model()
+    x_test, y_test = load_test_set()
+    evaluate(gtsrb_model, x_test, y_test)
--- utility.py
+++ utility.py
@@ -3,18 +3,23 @@ import numpy as np
 from sklearn import metrics
 
 
-def evaluate(model, test_dataset=None):
-    y_score = model.predict(test_dataset)
-    test_loss, test_acc = model.evaluate(test_dataset)
+def evaluate(model, x_test, y_test):
+    y_score = model.predict(x_test)
+    y_pred = np.argmax(y_score, axis=1)
+    y_true = np.argmax(y_test, axis=1)
+    correct_predictions = np.sum(np.equal(y_pred, y_true))
+    accuracy = correct_predictions / len(y_true)
+    print("accuracy: " + str(accuracy))
+
+    test_loss, test_acc = model.evaluate(x_test, y_test)
     print(f"Test accuracy: {test_acc:.3f}, test loss: {test_loss:.3f}")
-    y_pred = np.rint(y_score)  # to have 0 or 1
-    y_true = tf.concat([labels_batch for data_batch, labels_batch in test_dataset], axis=0)
+
     print("Classification report: ")
     print(type(y_true))
     print(type(y_pred))
-    print(y_pred.argmax(axis=1))
-    print(metrics.classification_report(y_true.numpy().argmax(axis=1), y_pred.argmax(axis=1), digits=4))
-    metrics.ConfusionMatrixDisplay.from_predictions(y_true.numpy().argmax(axis=1), y_pred.argmax(axis=1))
+    #print(y_pred.argmax(axis=1))
+    print(metrics.classification_report(y_true, y_pred, digits=4))
+    metrics.ConfusionMatrixDisplay.from_predictions(y_true, y_pred)
 
     # ROC curve
     '''fpr,tpr,th = metrics.roc_curve(y_true,y_score)
